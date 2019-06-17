2d03b01782147ca40f4ec52b4f2643c2ac5bf42c
Devin Martinolich <devin@Devins-MacBook-Pro.local>
2019-06-17 17:41:23 -0400

    first script to create a new laravel project with material dashboard

---
+++ new-laravel-material.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+PROJECTPATH='/Users/devin/projects/'
+
+echo -e "${BLUE}Creating new project: ${PROJECTPATH}$1${NC}"
+#create the project in our project folder
+cd ${PROJECTPATH}
+composer create-project --prefer-dist laravel/laravel $1
+
+echo -e "${BLUE}Installing material dashboard in $1${NC}"
+cd ${PROJECTPATH}$1
+composer require laravel-frontend-presets/material-dashboard
+
+echo -e "${BLUE}Setting material preset in $1${NC}"
+php artisan preset material
+
+echo -e "${BLUE}dumping composer autoload${NC}"
+composer dump-autoload
+
+echo -e "${BLUE}Copying the example .env${NC}"
+cp ${PROJECTPATH}$1/.env-example ${PROJECTPATH}$1/.env
+
+echo -e "${BLUE}Installation is complete.\nCreate your DB if you dont already have one.${NC}"
+
+echo -e "${BLUE}Once your .env is configured correctly dont forget\nto run: ${RED}php artisan migrate --seed${NC}"
